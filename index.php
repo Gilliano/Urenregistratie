e53c9b333b9813cdbb3c29f62d6c70daf21a3b4f
nielslucas <niels_lucas@hotmail.com>
2016-09-15 10:29:35 +0200

    Add basic includes

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-hoi
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login NEGG</title>
+
+	<!--Basic DLC-->
+	<link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<?php include 'php/main.php';?>
+
+	<!--Bootstrap-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/npm.js"></script>
+	
+</head>
+
+<body>
+
+</body>
+</html>
---
+++ php/main.php
@@ -0,0 +1,2 @@
+<?php
+echo "hello ik include php files";
