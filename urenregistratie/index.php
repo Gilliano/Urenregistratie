d53bc4a0da0e62b612fb296a2f9251c32c1a8c7b
LAPTOP-GKIAMBHJ\Erwin <erwinwernars@gmail.com>
2016-09-16 13:08:26 +0200

    De navigatiebalk, De opmaak van de navigatiebalk & het begin van de urenregistratie

---
+++ urenregistratie/index.php
@@ -0,0 +1,28 @@
+<?php include_once 'common/php/settings.php'; ?>
+
+<!DOCTYPE html>
+
+<html lang="nl">
+    <head>
+        <meta name="viewport" content="width=device-width,user-scalable=yes">
+        <link href="common/css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,400italic,700normal,700italic,700"/>
+        <link href="common/css/navbar.css" rel="stylesheet">
+        <title> <?= $s_Title ?></title>
+
+    </head>
+    <body>
+        <?php
+        include_once 'common/php/navbar.php';
+        ?>
+        <div class="container"> 
+            
+        </div>
+    </body>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</html>
