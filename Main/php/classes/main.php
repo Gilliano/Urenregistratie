3a1cfe65c499f8f11b5d169e7ad58afe84fe2c97
nielslucas <niels_lucas@hotmail.com>
2016-09-16 13:44:33 +0200

    index.php refactored + map structure refactored

---
+++ Main/php/classes/main.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Niels
+ * Date: 16-9-2016
+ * Time: 13:31
+ */
--- index.php
+++ index.php
@@ -1,25 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login NEGG</title>
-
-	<!--Basic DLC-->
-	<link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/>
-	<link rel="stylesheet" type="text/css" href="Main/css/main.css">
-	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,400italic,700normal,700italic,700"/>
-	<?php include 'Main/php/main.php';?>
-
-	<!--Bootstrap-->
-	<link rel="stylesheet" type="text/css" href="Main/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="Main/css/bootstrap-theme.min.css">
-	<script src="Main/js/bootstrap.min.js"></script>
-	<script src="Main/js/npm.js"></script>
-
+	<?php require_once'php/head.php'; ?>
 </head>
 
 <body>
 	<header>
-		<?php include'Main/php/navbar.php';?>
+		<?php include 'main/php/navbar.php';?>
 	</header>
 </body>
 </html>
