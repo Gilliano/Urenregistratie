0fd4f38e6647d68d93a4e24c5e61d1f39e8330c1
LAPTOP-GKIAMBHJ\Erwin <erwinwernars@gmail.com>
2016-09-21 14:14:40 +0200

    empty.txt verwijderd uit css, urenregistratie css toegevoegd en alle inline css uit urenregistratie index verwijderd.

--- urenregistratie/index.php
+++ urenregistratie/index.php
@@ -6,6 +6,7 @@
         <link href="../main/css/bootstrap.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,400italic,700normal,700italic,700"/>
         <link href="../main/css/navbar.css" rel="stylesheet">
+        <link href="css/urenregistratie.css" rel="stylesheet">
         <title>Branch</title>
 
     </head>
@@ -18,11 +19,11 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Kies een project</div>
                     <div class="panel-body">
-                        <form method="post" action="" id="form1" name="form1" enctype="multipart/form-data">
-                            <table style="margin-left: auto; margin-right: auto;">
+                        <form method="post" action="" name="project" enctype="multipart/form-data">
+                            <table>
                                 <tr>
-                                    <td width="600px">
-                                        <select style="width: 600px;" class="btn btn-default">
+                                    <td>
+                                        <select class="btn btn-default">
                                             <option selected disabled hidden>Kies een project</option>
                                             <option class="btn btn-default">Mustard</option>
                                             <option class="btn btn-default">Ketchup</option>
@@ -36,32 +37,32 @@
                 </div>
                 <div class="panel panel-default">
                     <div class="panel-heading">Uren invulformulier</div>
-                    <div class="panel-body">
-                        <form method="post" action="" name="urenformulier" enctype="multipart/form-data" oninput="(urentotaal.value=parseFloat(eindtijd.value)-parseFloat(begintijd.value))(ureninnovatief.value=parseFloat(urentotaal.value)-parseFloat(urenregulier.value))">
-                            <table style="margin-left: auto; margin-right: auto;">
-                                <tr style="border-bottom: 1px solid #D9D9D9; border-top: 1px solid #D9D9D9">
-                                    <td width="200px" height="50px" style="padding-left: 10px;">Begintijd</td>
-                                    <td width="400px"><input type="time" name="begintijd" class="form-control" id="begintijd"/></td>
+                    <div  class="panel-body">
+                        <form method="post" action="" id="urenformulier" name="urenformulier" enctype="multipart/form-data" oninput="(urentotaal.value=parseFloat(eindtijd.value)-parseFloat(begintijd.value))(ureninnovatief.value=parseFloat(urentotaal.value)-parseFloat(urenregulier.value))">
+                            <table>
+                                <tr>
+                                    <td class="description">Begintijd</td>
+                                    <td class="field"><input type="time" name="begintijd" class="form-control" id="begintijd"/></td>
                                 </tr>
-                                <tr style="border-bottom: 1px solid #D9D9D9; border-top: 1px solid #D9D9D9">
-                                    <td width="200px" height="50px" style="padding-left: 10px;">Eindtijd</td>
-                                    <td width="400px"><input type="time" name="eindtijd" class="form-control" id="eindtijd"/></td>
+                                <tr>
+                                    <td class="description">Eindtijd</td>
+                                    <td class="field"><input type="time" name="eindtijd" class="form-control" id="eindtijd"/></td>
                                 </tr>
-                                <tr style="border-bottom: 1px solid #D9D9D9; border-top: 1px solid #D9D9D9">
-                                    <td width="200px" height="50px" style="padding-left: 10px;">Totaal aantal uren gewerkt</td>
-                                    <td width="400px"><output readonly type="number" name="urentotaal" class="form-control" id="urentotaal"/></td>
+                                <tr>
+                                    <td class="description">Totaal aantal uren gewerkt</td>
+                                    <td class="field"><output readonly type="number" name="urentotaal" class="form-control" id="urentotaal"/></td>
                                 </tr>
-                                <tr style="border-bottom: 1px solid #D9D9D9; border-top: 1px solid #D9D9D9">
-                                    <td width="200px" height="50px" style="padding-left: 10px;">Reguliere uren</td>
-                                    <td width="400px"><input type="number" name="urenregulier" class="form-control" id="urenregulier"/></td>
+                                <tr>
+                                    <td class="description">Reguliere uren</td>
+                                    <td class="veld"><input type="number" name="urenregulier" class="form-control" id="urenregulier"/></td>
                                 </tr>
-                                <tr style="border-bottom: 1px solid #D9D9D9; border-top: 1px solid #D9D9D9">
-                                    <td width="200px" height="50px" style="padding-left: 10px;">Innovatieve uren</td>
-                                    <td width="400px"><output readonly type="number" name="ureninnovatief" class="form-control" id="ureninnovatief"/></td>
+                                <tr>
+                                    <td class="description">Innovatieve uren</td>
+                                    <td class="field"><output readonly type="number" name="ureninnovatief" class="form-control" id="ureninnovatief"/></td>
                                 </tr>
                                 <!--submit-->
                                 <tr>
-                                    <td height="50px" style="padding-left: 10px;"></td>
+                                    <td class="submit"></td>
                                     <td><input type="submit" name="urenopslaan" class="opslaan btn btn-success" value="Bevestigen"></td>
                                 </tr>
                             </table>
