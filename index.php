d64e90d03808dff84dea1814ff83b7956dff32f5
nielslucas <niels_lucas@hotmail.com>
2016-09-21 13:37:12 +0200

    changes index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php
-        //make sure everything we need in here
-        require_once 'main/php/head.php';
-        //Function for the login screen
-        userManager::login()
-    ?>
+index.php
 </head>
 
 <body>
