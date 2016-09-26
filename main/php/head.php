3ae7419c6afc99322cdc614f7c3a3e0a0bc8b19a
LAPTOP-GKIAMBHJ\Erwin <erwinwernars@gmail.com>
2016-09-26 19:24:51 +0200

    bootstrap select thema, geen standaard bootstrap maar werkt uitstekend! zelfs een live search en je kan ook meerdere dingen selecteren mocht dat nodig zijn! - toegepast in urenregistratie pagina.

--- main/php/head.php
+++ main/php/head.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,400italic,700normal,700italic,700"/>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 
 <!-- Navbar -->
 <link href="../main/css/navbar.css" rel="stylesheet">
@@ -17,6 +17,7 @@
 <!--Bootstrap-->
 <link rel="stylesheet" type="text/css" href="../main/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="../main/css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="../main/css/bootstrap-select.min.css">
 <script src="../main/js/bootstrap.min.js"></script>
 <script src="../main/js/npm.js"></script>
 
--- urenregistratie/index.php
+++ urenregistratie/index.php
@@ -11,41 +11,31 @@
 
     <?php
     $projectManager = new projectManager();
-    $size = sizeof($projectManager->getAllCurrentProject());
+    $size = sizeof($projectManager::getAllCurrentProject());
     ?>
 
     <body>
         <?php
             include_once '../main/php/navbar.php';
         ?>
-        <div class="container"> 
+        <div class="container">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Kies een project</div>
-                    <div class="panel-body">
-                        <form method="post" action="" name="project" enctype="multipart/form-data">
-                            <table>
-                                <tr>
-                                    <td>
-                                        <select class="btn btn-default">
-                                            <option selected disabled hidden>Kies een project</option>
-                                            <?php
-                                            foreach ($projectManager->getAllCurrentProject() as $value) {
-                                                echo "<option class=\"btn btn-default\">" . $value . "</option>";
-                                            }
-                                            ?>
-                                        </select>
-                                    </td>
-                                </tr>
-                            </table>
-                        </form>
-                    </div>
-                </div>
                 <div class="panel panel-default">
                     <div class="panel-heading">Uren invulformulier</div>
                     <div  class="panel-body">
                         <form method="post" action="" id="urenformulier" name="urenformulier" enctype="multipart/form-data" oninput="(urentotaal.value=parseFloat(eindtijd.value)-parseFloat(begintijd.value))(ureninnovatief.value=parseFloat(urentotaal.value)-parseFloat(urenregulier.value))">
                             <table>
+                                <tr>
+                                    <p>
+                                        <select class="selectpicker" data-width="700px" data-live-search="true" title="Kies een project...">
+                                            <?php
+                                            foreach ($projectManager::getAllCurrentProject() as $value) {
+                                                echo "<option>" . $value . "</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </p>
+                                </tr>
                                 <tr>
                                     <td class="description">Begintijd</td>
                                     <td class="field"><input type="time" name="begintijd" class="form-control" id="begintijd"/></td>
@@ -80,6 +70,12 @@
     </body>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
+
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/bootstrap-select.min.js"></script>
+
+    <!-- (Optional) Latest compiled and minified JavaScript translation files -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/i18n/defaults-*.min.js"></script>
 </html>
