cb7ecfae25cb45a683f6a4add47f20a99d44239e
Erwin Wernars <erwinwernars@gmail.com>
2016-10-13 00:23:53 +0200

    bug solved

--- urenregistratie/index.php
+++ urenregistratie/index.php
@@ -18,84 +18,8 @@
 
     $date = date("Y-m-d");
     ?>
-    
+
     <body>
-<<<<<<< HEAD
-   <?php
-      include_once '../main/php/navbar.php';
-      ?>
-   <div class="container">
-      <div class="col-md-12 switch">
-         <p>
-            <!--                <input type="checkbox" class="hidden" name="mode" id="mode" data-on-text="Advanced" data-off-text="Normal" data-toggle="modal" data-target="#modalFormulier">-->
-         </p>
-      </div>
-      <div class="col-md-8 col-md-offset-2">
-         <div class="panel panel-default" id="mainFormulier">
-            <div class="panel-heading">Uren invulformulier</div>
-            <div  class="panel-body">
-               <form method="post" action="" onsubmit="return submitChecker()" id="urenformulier" name="urenformulier" enctype="multipart/form-data">
-                  <table>
-                     <tr>
-                        <?php
-                           if(isset($_POST['urenopslaan'])) {
-                               echo urenManager::addUren();
-                           }
-                           ?>
-                        <td class="description">Project</td>
-                        <td class="field">
-                           <select class="selectpicker" name="project" data-width="" data-live-search="true" title="Kies een project..." required>
-                              <?php for($i = 0; $i < $arraySize; $i++) { ?>
-                              <option value="<?= $allCurrentProjects[$i]["idProject"]?>"><?= $allCurrentProjects[$i]["projectnaam"]?></option>
-                              <?php } ?>
-                           </select>
-                        </td>
-                     </tr>
-                     </tr>
-                     <tr>
-                        <td class="description">Datum</td>
-                        <td class="field"><input type="date" name="datum" class="form-control" id="datum" value="<?= $date ?>"/></td>
-                     </tr>
-                     <tr>
-                        <td class="description">Begintijd</td>
-                        <td class="field"><input type="time" onblur="realTimeWaarde()" name="begintijd" id="begintijd" step="1800" class="form-control" required/></td>
-                     </tr>
-                     <tr>
-                        <td class="description">Eindtijd</td>
-                        <td class="field"><input type="time" onblur="realTimeWaarde()" name="eindtijd" id="eindtijd" step="1800" class="form-control" required/></td>
-                     </tr>
-                     <tr>
-                        <td class="description">Totaal aantal uren gewerkt</td>
-                        <td class="field"><output readonly type="text" name="urentotaal" id="urentotaal" class="form-control"/></td>
-                     </tr>
-                     <tr>
-                        <td class="description">Reguliere uren</td>
-                        <td class="field"><input type="number" onkeyup="" name="urenregulier" id="urenregulier" onblur="urenRekenen()"  class="form-control" required/></td>
-                     </tr>
-                     <tr>
-                        <td class="description">Innovatieve uren</td>
-                        <td class="field"><output readonly type="text" name="ureninnovatief" id="ureninnovatief" value="3" class="form-control"/></td>
-                     </tr>
-                     <tr>
-                        <td class="description">Omschrijving van de uren</td>
-                        <td class="field"><textarea name="omschrijving" class="form-control" required/></textarea></td>
-                     </tr>
-                     <!--submit-->
-                     <tr>
-                        <td class="submit"></td>
-                        <td><input type="submit" name="urenopslaan" id="urenopslaan" class="opslaan btn btn-success" value="Bevestigen"></td>
-                     </tr>
-                  </table>
-               </form>
-            </div>
-         </div>
-      </div>
-   </div>
-   <!-- Here comes the jQuery model output -->
-   <div class="col-md-8 col-md-offset-2" id="modalContent">
-   </div>
-</body>
-=======
     <?php
     include_once '../main/php/navbar.php';
     ?>
@@ -134,11 +58,11 @@
                         </tr>
                             <tr>
                                 <td class="description">Begintijd</td>
-                                <td class="field"><input type="time" onblur="realTimeWaarde(); urenInnovatief()" name="begintijd" id="begintijd" step="1800" class="form-control" required/></td>
+                                <td class="field"><input type="time" onblur="realTimeWaarde()" name="begintijd" id="begintijd" step="1800" class="form-control" required/></td>
                             </tr>
                             <tr>
                                 <td class="description">Eindtijd</td>
-                                <td class="field"><input type="time" onblur="realTimeWaarde(); urenInnovatief()" name="eindtijd" id="eindtijd" step="1800" class="form-control" required/></td>
+                                <td class="field"><input type="time" onblur="realTimeWaarde()" name="eindtijd" id="eindtijd" step="1800" class="form-control" required/></td>
                             </tr>
                             <tr>
                                 <td class="description">Totaal aantal uren gewerkt</td>
@@ -146,7 +70,7 @@
                             </tr>
                             <tr>
                                 <td class="description">Reguliere uren</td>
-                                <td class="field"><input type="number" oninput="urenInnovatief()" id="urenregulier" name="urenregulier" step="0.1" class="form-control" required/></td>
+                                <td class="field"><input type="number" onkeyup="urenInnovatief()" id="urenregulier" name="urenregulier" class="form-control" required/></td>
                             </tr>
                             <tr>
                                 <td class="description">Innovatieve uren</td>
@@ -251,7 +175,6 @@
 
     </div>
     </body>
->>>>>>> 886ee1765cd0a36bd9343302b3a535b8cb9edb2e
     <?php
 
     //performance increasing not everything in the head, not necessary things in the footer.
