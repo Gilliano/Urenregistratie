1bf16b5623223d380610f7586a52781bfdeed04e
Mohammed Belhaj <m.belhaj_zakelijk@hotmail.com>
2016-10-12 22:35:11 +0200

    urenregistratie pagina ultimate fix. modal moet aangeroepen worden en een andere pagina moet gezien laten worden

--- urenregistratie/index.php
+++ urenregistratie/index.php
@@ -16,163 +16,82 @@
     $alleMedewerkers = sizeof(userManager::getAllUsers());
     $date = date("Y-m-d");
     ?>
-
+    
     <body>
-    <?php
-    include_once '../main/php/navbar.php';
-    ?>
-    <div class="container">
-        <div class="col-md-12 switch">
-            <p>
-<!--                <input type="checkbox" class="hidden" name="mode" id="mode" data-on-text="Advanced" data-off-text="Normal" data-toggle="modal" data-target="#modalFormulier">-->
-            </p>
-        </div>
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default" id="mainFormulier">
-                <div class="panel-heading">Uren invulformulier</div>
-                <div  class="panel-body">
-                    <form method="post" action="" id="urenformulier" name="urenformulier" enctype="multipart/form-data" oninput="(ureninnovatief.value=parseFloat(urentotaal.value)-parseFloat(urenregulier.value))">
-                        <table>
-                            <tr>
-                                <?php
-                                if(isset($_POST['urenopslaan'])) {
-                                    echo urenManager::addUren();
-                                }
-                                ?>
-                                <td class="description">Project</td>
-                                <td class="field">
-
-                                    <select class="selectpicker" name="project" data-width="" data-live-search="true" title="Kies een project..." required>
-                                        <?php for($i = 0; $i < $arraySize; $i++) { ?>
-                                            <option value="<?= $allCurrentProjects[$i]["idProject"]?>"><?= $allCurrentProjects[$i]["projectnaam"]?></option>
-                                        <?php } ?>
-                                    </select>
-                                </td>
-                            </tr>
- 						</tr>
-                        <tr>
-                            <td class="description">Datum</td>
-                            <td class="field"><input type="date" name="datum" class="form-control" id="datum" value="<?= $date ?>"/></td>
-                        </tr>
-                            <tr>
-                                <td class="description">Begintijd</td>
-                                <td class="field"><input type="time" onblur="realTimeWaarde()" name="begintijd" id="begintijd" step="1800" class="form-control" required/></td>
-                            </tr>
-                            <tr>
-                                <td class="description">Eindtijd</td>
-                                <td class="field"><input type="time" onblur="realTimeWaarde()" name="eindtijd" id="eindtijd" step="1800" class="form-control" required/></td>
-                            </tr>
-                            <tr>
-                                <td class="description">Totaal aantal uren gewerkt</td>
-                                <td class="field"><output readonly type="number" name="urentotaal" id="urentotaal" class="form-control"/></td>
-                            </tr>
-                            <tr>
-                                <td class="description">Reguliere uren</td>
-                                <td class="veld"><input type="number" name="urenregulier" class="form-control" required/></td>
-                            </tr>
-                            <tr>
-                                <td class="description">Innovatieve uren</td>
-                                <td class="field"><input type="number" name="ureninnovatief" class="form-control" readonly/></td>
-                            </tr>
-                            <tr>
-                                <td class="description">Omschrijving van de uren</td>
-                                <td class="field"><textarea name="omschrijving" class="form-control" required/></textarea></td>
-                            </tr>
-                            <!--submit-->
-                            <tr>
-                                <td class="submit"></td>
-                                <td><input type="submit" name="urenopslaan" class="opslaan btn btn-success" value="Bevestigen"></td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
+   <?php
+      include_once '../main/php/navbar.php';
+      ?>
+   <div class="container">
+      <div class="col-md-12 switch">
+         <p>
+            <!--                <input type="checkbox" class="hidden" name="mode" id="mode" data-on-text="Advanced" data-off-text="Normal" data-toggle="modal" data-target="#modalFormulier">-->
+         </p>
+      </div>
+      <div class="col-md-8 col-md-offset-2">
+         <div class="panel panel-default" id="mainFormulier">
+            <div class="panel-heading">Uren invulformulier</div>
+            <div  class="panel-body">
+               <form method="post" action="" onsubmit="return submitChecker()" id="urenformulier" name="urenformulier" enctype="multipart/form-data">
+                  <table>
+                     <tr>
+                        <?php
+                           if(isset($_POST['urenopslaan'])) {
+                               echo urenManager::addUren();
+                           }
+                           ?>
+                        <td class="description">Project</td>
+                        <td class="field">
+                           <select class="selectpicker" name="project" data-width="" data-live-search="true" title="Kies een project..." required>
+                              <?php for($i = 0; $i < $arraySize; $i++) { ?>
+                              <option value="<?= $allCurrentProjects[$i]["idProject"]?>"><?= $allCurrentProjects[$i]["projectnaam"]?></option>
+                              <?php } ?>
+                           </select>
+                        </td>
+                     </tr>
+                     </tr>
+                     <tr>
+                        <td class="description">Datum</td>
+                        <td class="field"><input type="date" name="datum" class="form-control" id="datum" value="<?= $date ?>"/></td>
+                     </tr>
+                     <tr>
+                        <td class="description">Begintijd</td>
+                        <td class="field"><input type="time" onblur="realTimeWaarde()" name="begintijd" id="begintijd" step="1800" class="form-control" required/></td>
+                     </tr>
+                     <tr>
+                        <td class="description">Eindtijd</td>
+                        <td class="field"><input type="time" onblur="realTimeWaarde()" name="eindtijd" id="eindtijd" step="1800" class="form-control" required/></td>
+                     </tr>
+                     <tr>
+                        <td class="description">Totaal aantal uren gewerkt</td>
+                        <td class="field"><output readonly type="text" name="urentotaal" id="urentotaal" class="form-control"/></td>
+                     </tr>
+                     <tr>
+                        <td class="description">Reguliere uren</td>
+                        <td class="field"><input type="number" onkeyup="" name="urenregulier" id="urenregulier" onblur="urenRekenen()"  class="form-control" required/></td>
+                     </tr>
+                     <tr>
+                        <td class="description">Innovatieve uren</td>
+                        <td class="field"><output readonly type="text" name="ureninnovatief" id="ureninnovatief" value="3" class="form-control"/></td>
+                     </tr>
+                     <tr>
+                        <td class="description">Omschrijving van de uren</td>
+                        <td class="field"><textarea name="omschrijving" class="form-control" required/></textarea></td>
+                     </tr>
+                     <!--submit-->
+                     <tr>
+                        <td class="submit"></td>
+                        <td><input type="submit" name="urenopslaan" id="urenopslaan" class="opslaan btn btn-success" value="Bevestigen"></td>
+                     </tr>
+                  </table>
+               </form>
             </div>
-        </div>
-    </div>
-
-    <!-- Here comes the jQuery model output -->
-    <div class="col-md-8 col-md-offset-2" id="modalContent">
-    </div>
-
-
-
-
-    <!-- This is a pop-up. When switch to advanced mode this screen will been show -->
-    <div class="container">
-        <!-- Modal -->
-        <div class="modal fade modalFormulier" id="modalFormulier" role="dialog">
-            <div class="modal-dialog">
-
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Team uren invullen</h4>
-                    </div>
-                    <div class="modal-body">
-                        <form method="post" action="" id="urenformulier" name="urenformulier" enctype="multipart/form-data" oninput="(urentotaal.value=parseFloat(eindtijd.value)-parseFloat(begintijd.value))(ureninnovatief.value=parseFloat(urentotaal.value)-parseFloat(urenregulier.value))">
-                            <table>
-                                <tr>
-                                    <td class="description">Medewerkers</td>
-                                    <td class="field">
-                                        <select class="selectpicker" multiple name="medewerker" id="medewerker" data-width="" data-live-search="true" title="Kies medewerkers...">
-                                            <?php for($i = 0; $i < $alleMedewerkers; $i++) { ?>
-                                                <?php if(empty($allUsers[$i]["tussenvoegsel"])) { ?>
-                                                    <option value="<?= $allUsers[$i]["idMedewerker"] ?>"><?= $allUsers[$i]["voornaam"] . " "  . $allUsers[$i]["achternaam"]?></option>
-                                                <?php } else{  ?>
-                                                    <option value="<?= $allUsers[$i]["idMedewerker"] ?>"><?= $allUsers[$i]["voornaam"] . " " . $allUsers[$i]["tussenvoegsel"] . " " . $allUsers[$i]["achternaam"]?></option>
-                                                <?php } } ?>
-                                        </select>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="description">Project</td>
-                                    <td class="field">
-                                        <select class="selectpicker" name="project" id="project" data-width="" data-live-search="true" title="Kies een project...">
-                                            <?php for($i = 0; $i < $arraySize; $i++) { ?>
-                                                <option value="<?= $allCurrentProjects[$i]["idProject"]?>"><?= $allCurrentProjects[$i]["projectnaam"]?></option>
-                                            <?php } ?>
-                                        </select>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="description">Begintijd</td>
-                                    <td class="field"><input type="time" name="begintijd" id="begintijd" class="form-control" required/></td>
-                                </tr>
-                                <tr>
-                                    <td class="description">Eindtijd</td>
-                                    <td class="field"><input type="time" name="eindtijd" id="eindtijd" class="form-control"  required/></td>
-                                </tr>
-                                <tr>
-                                    <td class="description">Totaal aantal uren gewerkt</td>
-                                    <td class="field"><output readonly type="number" name="urentotaal" id="urentotaal" class="form-control"/></td>
-                                </tr>
-                                <tr>
-                                    <td class="description">Reguliere uren</td>
-                                    <td class="veld"><input type="number" name="urenregulier" id="urenregulier" class="form-control" required/></td>
-                                </tr>
-                                <tr>
-                                    <td class="description">Innovatieve uren</td>
-                                    <td class="field"><input type="number" name="ureninnovatief" id="ureninnovatief" class="form-control" readonly/></td>
-                                </tr>
-                                <tr>
-                                    <td class="description">Omschrijving van de uren</td>
-                                    <td class="field"><textarea name="omschrijving" id="omschrijving"  class="form-control" required/></textarea></td>
-                                </tr>
-                            </table>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <input type="submit" name="urenopslaan" class="opslaan btn btn-success" value="Bevestigen">
-                        <input type="submit" name="urenopslaan" class="opslaan btn btn-danger" data-dismiss="modal" value="Annuleren">
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-    </div>
-    </body>
+         </div>
+      </div>
+   </div>
+   <!-- Here comes the jQuery model output -->
+   <div class="col-md-8 col-md-offset-2" id="modalContent">
+   </div>
+</body>
     <?php
 
     //performance increasing not everything in the head, not necessary things in the footer.
--- urenregistratie/js/urenAfronden.js
+++ urenregistratie/js/urenAfronden.js
@@ -1,67 +1,17 @@
+
 function aantalUren(begintijd, eindtijd){
 
     //split de waardes in een array met 2 gegevens 10:33  is    b[0] = 10 en b[1] = 33
-    var b = begintijd.split(':');
-    var e = eindtijd.split(':');
+    var b = splitter(begintijd);
+    var e = splitter(eindtijd);
+
 
     //de tijden worden veranderd naar seconden
     var begintijd_seconden = b[0] * 3600  +  b[1] * 60;
     var eindtijd_seconden = (+e[0]) * 3600  +  (+e[1]) * 60;
 
     // Uren afronden specifiek op halve en hele uren
-    if(b[1] < 15) {
-        b[0] = parseInt(b[0]);
-        b[0] = b[0].toString();
-        b[1] = "00";
-        var b = b.toString();
-        b = b.replace(",", ":");
-        document.getElementById("begintijd").value = b;
-    }
-    else if(b[1] >= 15 && b[1] <= 44) {
-        b[0] = parseInt(b[0]);
-        b[0] = b[0].toString();
-        b[1] = "30";
-        var b = b.toString();
-        b = b.replace(",", ":");
-        document.getElementById("begintijd").value = b;
-    }
-    else {
-        b[0] = parseInt(b[0]);
-        b[0] += 1;
-        b[0] = b[0].toString();
-        b[1] = "00";
-        var b = b.toString();
-        b = b.replace(",", ":");
-        document.getElementById("begintijd").value = b;
-    }
-
-    // Hetzelfde als bovenstaande if statements maar dan voor eindtijd
-    if(e[1] < 15) {
-        e[0] = parseInt(e[0]);
-        e[0] = e[0].toString();
-        e[1] = "00";
-        var e = e.toString();
-        e = e.replace(",", ":");
-        document.getElementById("eindtijd").value = e;
-    }
-    else if(e[1] >= 15 && e[1] <= 44) {
-        e[0] = parseInt(e[0]);
-        e[0] = e[0].toString();
-        e[1] = "30";
-        var e = e.toString();
-        e = e.replace(",", ":");
-        document.getElementById("eindtijd").value = e;
-    }
-    else {
-        e[0] = parseInt(e[0]);
-        e[0] += 1;
-        e[0] = e[0].toString();
-        e[1] = "00";
-        var e = e.toString();
-        e = e.replace(",", ":");
-
-        document.getElementById("eindtijd").value = e;
-    }
+    
 
 
     //eindtijd min begintijd
@@ -71,7 +21,6 @@ function aantalUren(begintijd, eindtijd){
 
     //hier maken we een decimaal van de eindcijfer in dit geval 1 getal achter de komma
     var uren = parseFloat(eind.toFixed(1));
-    var uren = uren.toString();
 
     if(uren < 0 ) {
         return "begintijd is groter dan eindtijd!";
@@ -80,15 +29,136 @@ function aantalUren(begintijd, eindtijd){
         return "U mag geen 0 uren hebben";
     }
     else{
-        uren = uren.replace(".",",");
         return uren;
+    }
+
+}
+
+function splitter(tijd){
+    
+    var tijd = tijd.split(':');
+    return tijd;
+}
+function tijdAfronden(tijd){
+
+    var tijd = splitter(tijd);
+
+    if(tijd[1] >= 15 && tijd[1] <= 44) {
+        tijd[1] = "30";
+        var tijd = (tijd[0] + ':' + tijd[1]);
+        return tijd;
+    }
+    else if(tijd[1] >=45 && tijd[1] <=59){
+        
+        if(parseInt(tijd[0]) < 10){
+            tijd[0] = ('0' + (parseInt(tijd[0]) + 1).toString()); 
+            console.log(tijd[0]);
+        }
+        else{
+            tijd[0] = (parseInt(tijd[0]) + 1).toString();
+            console.log(tijd[0]); 
+        } 
+        tijd[1] = "00";
+        var tijd = (tijd[0] + ':' + tijd[1]);
+        return tijd;
+    }
+    else {
+        tijd[1] = "00";
+        var tijd = (tijd[0] + ':' + tijd[1]);
+        return tijd;
     }
 
 }
 
 //zet de waarde in realtime in de div id="return" haalt de waarde begintijd en eindtijd op van de input velden
 function realTimeWaarde() {
-    var begintijd = document.getElementById("begintijd");
-    var eindtijd = document.getElementById("eindtijd");
-    document.getElementById("urentotaal").innerHTML = aantalUren(begintijd.value, eindtijd.value);
+    var begintijd = document.getElementById("begintijd").value;
+    var eindtijd = document.getElementById("eindtijd").value;   
+
+    if(begintijd != ""){
+        document.getElementById("begintijd").value = tijdAfronden(begintijd);
+    }
+    if(eindtijd != ""){
+        document.getElementById("eindtijd").value = tijdAfronden(eindtijd);
+    }
+
+    if(begintijd != "" && eindtijd != ""){
+    document.getElementById("urentotaal").innerHTML = aantalUren(tijdAfronden(begintijd), tijdAfronden(eindtijd));
+    }
+}
+
+function ureninnovatief(urentotaal, urenregulier){
+        
+        uren = urentotaal - urenregulier;
+
+   
+    if(!isNumeric(urentotaal)){
+        return 'de gegevens kloppen niet';
+
+    }
+    else if(uren < 0){
+        return 'u heeft teveel uren ingevuld bij reguliere uren';
+    }
+    else if(uren >= 0){
+    return uren;
+    }
+}
+function submitChecker() {
+    begintijd = document.getElementById("begintijd").value;
+    eindtijd = document.getElementById("eindtijd").value;   
+    urenregulier = document.getElementById("urenregulier").value;
+    urentotaal = document.getElementById("urentotaal").value;
+
+    if(errorController() == false){
+        alert('U heeft verkeerde velden die aangepast moeten worden');
+        return false;
+
+    }
+    else{
+        return true;
+    }
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function errorController(){
+    // date > vandaag
+    // eindtijd > begintijd
+    // reguliere uren < totaal
+    begintijd = document.getElementById("begintijd").value;
+    eindtijd = document.getElementById("eindtijd").value;   
+    urenregulier = document.getElementById("urenregulier").value;
+    urentotaal = document.getElementById("urentotaal").value;
+
+    //controleer innovatieve uren
+    if(!isNumeric(ureninnovatief(urentotaal,urenregulier))){
+        return false;
+    }
+    else if(!isNumeric(aantalUren(begintijd, eindtijd))){
+        return false;
+    }
+    else{
+        return true;
+    }
+    
+    //
+
+
+}
+
+function urenRekenen(){
+
+    urenregulier = document.getElementById("urenregulier").value;
+    urentotaal = document.getElementById("urentotaal").value;
+
+    if(urenregulier != "" && urentotaal != ""){
+    document.getElementById("ureninnovatief").innerHTML = ureninnovatief(urentotaal,urenregulier);
+    }
+}
+
+function buttonChecker(bool){
+    
+    document.getElementById("urenopslaan").disabled = bool; 
 }
