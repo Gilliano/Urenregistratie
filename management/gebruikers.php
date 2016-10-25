9372badf47f6a627eb59569f0391d70817291301
Niels Lucas <niels_lucas@hotmail.com>
2016-10-25 14:33:52 +0200

    Fixed managemet part where you can change users data as admin

--- management/gebruikers.php
+++ management/gebruikers.php
@@ -14,50 +14,14 @@
 <td colspan="3" valign="top" class='tabbed-content-container ui-corner-all'>
     <tr>
         <td>
-            <button id= "registreren" class="btn btn-secondary btn-sm registreren" value =  "1">Registreren</button>
-            <button id= "wijzigen" class="btn btn-secondary btn-sm wijzigen" value =  "1">Wijzigen</button>
-<!--            <button id= "Close-open-button-1" class="btn btn-secondary btn-sm Close-open-button-1" value =  "1">Nog iets</button>-->
+            <button id="wijzigen" class="btn btn-secondary btn-sm wijzigen" value="1">Wijzigen</button>
+            <button id="registreren" class="btn btn-secondary btn-sm registreren" value="1">Registreren</button>
+            <!--            <button id= "Close-open-button-1" class="btn btn-secondary btn-sm Close-open-button-1" value =  "1">Nog iets</button>-->
         </td>
     </tr>
 
-    <div class="registreren">
-        <form id="management_register_user" method="post" role="login">
-            <h4 class="modal-title">Registreren</h4>
 
-            <input type="text" name="firstname" placeholder="Voornaam" autocomplete="false" required class="form-control input-lg" />
-
-            <input type="text" name="insertion" placeholder="Tussenvoegsel" autocomplete="false" class="form-control input-lg" />
-
-            <input type="text" name="lastname" placeholder="Achternaam" autocomplete="false" required class="form-control input-lg" />
-
-            <div class="row">
-                <div class="col-sm-6 startmail">
-                    <input type="text" name="email" placeholder="Email" autocomplete="false" required class="form-control input-lg" />
-                </div>
-                <div class="col-sm-6 endmail">
-                    <p>@branchonline.nl</p>
-                </div>
-            </div>
-
-            <input type="password" name="password" class="form-control input-lg" autocomplete="off" id="password" placeholder="Wachtwoord"/>
-            <input type="password" name="repassword" class="form-control input-lg" autocomplete="off" id="password" placeholder="Herhaal wachtwoord"/>
-
-            <input type="submit" name="user_register" value="Registreer" class="btn btn-lg btn-primary btn-block">
-
-            <?php
-            if(userManager::register() === false) {
-                $error = "";
-                $error .= "<div class='alert alert-danger'>";
-                $error .= "Er is iets mis gegaan";
-                $error .="</div>";
-
-                echo $error;
-            }
-            ?>
-        </form>
-    </div>
-
-    <div style="display: none" class="wijzigen div-1">
+    <div class="wijzigen div-1">
         <table id="allUsersTable" class="gebruikers_wijzigen table">
             <t>
                 <td><h4>Voornaam</h4></td>
@@ -68,7 +32,7 @@
                 <td><h4>Rol</h4></td>
                 <td><h4>Status</h4></td>
             </t>
-            <?php echo users();?>
+            <?php echo users(); ?>
         </table>
 
     </div>
@@ -86,21 +50,28 @@
                 <div class="modal-body">
                     <form method="post" id="gebruiker_wijzig_form" style="margin: auto" role="login">
                         <h4 class="modal-title">Registreren</h4>
-                        <input type="hidden" name="id" autocomplete="false" required class="form-control input-lg id" />
+                        <input type="hidden" name="id" autocomplete="false" required class="form-control input-lg id"/>
 
-                        <input type="text" name="firstname" placeholder="Voornaam" autocomplete="false" required class="form-control input-lg firstname" />
+                        <input type="text" name="firstname" placeholder="Voornaam" autocomplete="false" required
+                               class="form-control input-lg firstname"/>
 
-                        <input type="text" name="insertion" placeholder="Tussenvoegsel" autocomplete="false" class="form-control input-lg tussenvoegsel" />
+                        <input type="text" name="insertion" placeholder="Tussenvoegsel" autocomplete="false"
+                               class="form-control input-lg tussenvoegsel"/>
 
-                        <input type="text" name="lastname" placeholder="Achternaam" autocomplete="false" required class="form-control input-lg lastname" />
+                        <input type="text" name="lastname" placeholder="Achternaam" autocomplete="false" required
+                               class="form-control input-lg lastname"/>
 
-                        <input type="text" name="email" placeholder="Email" autocomplete="false" required class="form-control input-lg email" />
+                        <input type="text" name="email" placeholder="Email" autocomplete="false" required
+                               class="form-control input-lg email"/>
 
-                        <input type="text" name="valid" placeholder="Valid" autocomplete="false" required class="form-control input-lg valid" />
+                        <input type="text" name="valid" placeholder="Valid" autocomplete="false" required
+                               class="form-control input-lg valid"/>
 
-                        <input type="text" name="rol" placeholder="Rol" autocomplete="false" required class="form-control input-lg rol" />
+                        <input type="text" name="rol" placeholder="Rol" autocomplete="false" required
+                               class="form-control input-lg rol"/>
 
-                        <input type="text" name="status" placeholder="status" autocomplete="false" required class="form-control input-lg status" />
+                        <input type="text" name="status" placeholder="status" autocomplete="false" required
+                               class="form-control input-lg status"/>
 
                     </form>
                 </div>
@@ -113,10 +84,53 @@
         </div>
     </div>
 
-<!--    <div style="display: none" class="Close-div-1 div-1">-->
-<!--        <p> Close - TBC</p>-->
-<!--        <button id="Hide-1"    class=" Hide-1">hide</button>-->
-<!--    </div>-->
+    <div class="registreren" style="display: none">
+        <form id="management_register_user" method="post" role="login">
+            <h4 class="modal-title">Registreren</h4>
+
+            <input type="text" name="firstname" placeholder="Voornaam" autocomplete="false" required
+                   class="form-control input-lg"/>
+
+            <input type="text" name="insertion" placeholder="Tussenvoegsel" autocomplete="false"
+                   class="form-control input-lg"/>
+
+            <input type="text" name="lastname" placeholder="Achternaam" autocomplete="false" required
+                   class="form-control input-lg"/>
+
+            <div class="row">
+                <div class="col-sm-6 startmail">
+                    <input type="text" name="email" placeholder="Email" autocomplete="false" required
+                           class="form-control input-lg"/>
+                </div>
+                <div class="col-sm-6 endmail">
+                    <p>@branchonline.nl</p>
+                </div>
+            </div>
+
+            <input type="password" name="password" class="form-control input-lg" autocomplete="off" id="password"
+                   placeholder="Wachtwoord"/>
+            <input type="password" name="repassword" class="form-control input-lg" autocomplete="off" id="password"
+                   placeholder="Herhaal wachtwoord"/>
+
+            <input type="submit" name="user_register" value="Registreer" class="btn btn-lg btn-primary btn-block">
+
+            <?php
+            //            if(userManager::register() === false) {
+            //                $error = "";
+            //                $error .= "<div class='alert alert-danger'>";
+            //                $error .= "Er is iets mis gegaan";
+            //                $error .="</div>";
+            //
+            //                echo $error;
+            //            }
+            ?>
+        </form>
+    </div>
+
+    <!--    <div style="display: none" class="Close-div-1 div-1">-->
+    <!--        <p> Close - TBC</p>-->
+    <!--        <button id="Hide-1"    class=" Hide-1">hide</button>-->
+    <!--    </div>-->
 
 
 
--- management/php/management.php
+++ management/php/management.php
@@ -6,42 +6,44 @@
  * Time: 17:11
  */
 
-function users() {
+function users()
+{
     $users = userManager::getAllUsers();
     $table = '';
     foreach ($users as $user) {
 
         $table .= "<tr>";
-        $table .=   "<td style='display: none'>".$user['idMedewerker']."</td>";
-        $table .=   "<td>".$user['voornaam']."</td>";
-        $table .=   '<td>'.$user['tussenvoegsels'].'</td>';
-        $table .=   '<td>'.$user['achternaam'].'</td>';
-        $table .=   '<td>'.$user['email'].'</td>';
-        $table .=   '<td>'.$user['validated'].'</td>';
-        $table .=   '<td>'.$user['rol'].'</td>';
-        $table .=   '<td>'.$user['state'].'</td>';
-        $table .=   "<td> <button type='submit' name='gebruiker_wijzig' value='". $user['idMedewerker'] ."' class='btn btn-default' data-toggle='modal' data-target='#myModal'>Wijzig</button> </td>";
+        $table .= "<td style='display: none'>" . $user['idMedewerker'] . "</td>";
+        $table .= "<td>" . $user['voornaam'] . "</td>";
+        $table .= '<td>' . $user['tussenvoegsels'] . '</td>';
+        $table .= '<td>' . $user['achternaam'] . '</td>';
+        $table .= '<td>' . $user['email'] . '</td>';
+        $table .= '<td>' . $user['validated'] . '</td>';
+        $table .= '<td>' . $user['rol'] . '</td>';
+        $table .= '<td>' . $user['state'] . '</td>';
+        $table .= "<td> <button type='submit' id='gebruiker_wijzig' name='gebruiker_wijzig' value='" . $user['idMedewerker'] . "' class='btn btn-default' data-toggle='modal' data-target='#myModal'>Wijzig</button> </td>";
         $table .= '</tr>';
 
-        }
+    }
 
     return $table;
 }
 
-function projecten() {
+function projecten()
+{
     $projecten = projectManager::getAllProjects();
     $table = '';
     foreach ($projecten as $project) {
 
         $table .= "<tr>";
 
-        $table .=   "<td>".$project['projectnaam']."</td>";
-        if($project['verwijderd'] == 0) {
-            $table .=   '<td>Niet verwijderd</td>';
+        $table .= "<td>" . $project['projectnaam'] . "</td>";
+        if ($project['verwijderd'] == 0) {
+            $table .= '<td>Niet verwijderd</td>';
         } else {
-            $table .=   '<td>Wel verwijderd</td>';
+            $table .= '<td>Wel verwijderd</td>';
         }
-        $table .=   "<td><button type='submit' name='gebruiker_wijzig' value='". $project['idProject'] ."' class='btn btn-default' data-toggle='modal' data-target='#myModal'>toggle status</button></td>";
+        $table .= "<td><button type='submit' name='gebruiker_wijzig' value='" . $project['idProject'] . "' class='btn btn-default' data-toggle='modal' data-target='#myModal'>toggle status</button></td>";
         $table .= '</tr>';
 
     }
