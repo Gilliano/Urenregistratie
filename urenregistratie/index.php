5ee282ccac749acf3836ec5f0a93bd8cd1901762
Erwin Wernars <erwinwernars@gmail.com>
2016-11-03 23:24:48 +0100

    date & time input works in firefox now

--- urenregistratie/index.php
+++ urenregistratie/index.php
@@ -180,7 +180,6 @@
 
             </div>
         </div>
-
     </div>
     </body>
     <?php
@@ -194,4 +193,17 @@
     <script src="js/modalHandler.js"></script>
     <!-- Load hour calculating -->
     <script src="js/urenAfronden.js"></script>
+
+
+    <!-- cdn for modernizr, if you haven't included it already -->
+    <script src="http://cdn.jsdelivr.net/webshim/1.12.4/extras/modernizr-custom.js"></script>
+    <!-- polyfiller file to detect and load polyfills -->
+    <script src="http://cdn.jsdelivr.net/webshim/1.12.4/polyfiller.js"></script>
+    <script>
+        webshims.setOptions('waitReady', false);
+        webshims.setOptions('forms-ext', {types: 'date'});
+        webshims.polyfill('forms forms-ext');
+    </script>
+
+
 </html>
